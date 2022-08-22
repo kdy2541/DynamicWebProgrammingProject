4b7a46dda63cf614060de0a24bb70052b3034e5d
강동연 <51121837+kdy2541@users.noreply.github.com>
2022-08-22 15:04:02 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,50 +1,50 @@
 let songs = [
     {
         name: 'song 1',
-        path: 'assets/musics/1.mp3',
+        path: './musics/1.mp3',
         artist: 'artist 1',
-        cover: './assets/img/1.jpg'
+        cover: './/img/1.jpg'
     },
     {
         name: 'song 2',
-        path: 'assets/musics/2.mp3',
+        path: './musics/2.mp3',
         artist: 'artist 2',
-        cover: './assets/img/2.jpg'
+        cover: './img/2.jpg'
     },
     {
         name: 'song 3',
-        path: 'assets/musics/3.mp3',
+        path: './musics/3.mp3',
         artist: 'artist 3',
-        cover: './assets/img/3.jpg'
+        cover: './img/3.jpg'
     },
     {
         name: 'song 4',
-        path: 'assets/musics/4.mp3',
+        path: './musics/4.mp3',
         artist: 'artist 4',
-        cover: './assets/img/4.jpg'
+        cover: './img/4.jpg'
     },
     {
         name: 'song 5',
-        path: 'assets/musics/5.mp3',
+        path: './musics/5.mp3',
         artist: 'artist 5',
-        cover: './assets/img/5.jpg'
+        cover: './img/5.jpg'
     },
     {
         name: 'song 6',
-        path: 'assets/musics/6.mp3',
+        path: './musics/6.mp3',
         artist: 'artist 6',
-        cover: './assets/img/6.jpg'
+        cover: './img/6.jpg'
     },
     {
         name: 'song 7',
-        path: 'assets/musics/7.mp3',
+        path: './musics/7.mp3',
         artist: 'artist 7',
-        cover: './assets/img/7.jpg'
+        cover: './img/7.jpg'
     },
     {
         name: 'song 8',
-        path: 'assets/musics/8.mp3',
+        path: './musics/8.mp3',
         artist: 'artist 8',
-        cover: './assets/img/8.jpg'
+        cover: './img/8.jpg'
     }
 ]
